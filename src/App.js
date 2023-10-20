25aff4fa7b666fedebe7498d2114eaac358080e3
abdur rehman <abed.javed12@gmail.com>
2023-10-21 04:21:25 +0500

    myweb

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { useState } from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+  const[data,setData] = useState('');
+  const[value,setValue] = useState('');
+   
+ 
+
+ 
+
+  const onClick= (e) => {
+    
+    if(data.trim().length >0 ){
+      alert(data);
+    }
+     else{
+     if(value.trim().length > 0){
+      alert(value);
+     }
+  }
+  
+
+  }
+  
+  
+
+  
+  return (
+    <div>
+      
+      
+      <input type='text'
+      
+      value={data}
+      onChange={(e)=>setData(e.target.value)}
+      
+      
+      />
+      <input  type="text" value={value} onChange={(e) =>setValue (e.target.value)} />
+
+      <buttn onClick={onClick}>  submit</buttn>
+      
+      
+      
+      
+    </div>
+  )
+}
+  
 export default App;
+
+
+
+
+
